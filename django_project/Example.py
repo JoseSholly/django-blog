ce1b4eeacf8568a76a5451aad3dcfba4c8fe6b45
JoseSholly <josepholawuyi30@gmail.com>
2023-08-05 12:49:31 +0100

    Update

---
+++ django_project/Example.py
@@ -0,0 +1,11 @@
+import os
+
+# print(os.environ.get("DEFAULT_EMAIL"))
+
+
+# os.environ["MAILJET_API_KEY"]= 'd6b1794ecc3a715a07f8fe823d8c540c'
+# os.environ["MAILJET_SECRET_KEY"]= '6c1a96ff465c2b69c264d6db824589f2'
+# os.environ["MAILJET_API_URL"]= 'https://api.mailjet.com/v3.1/'
+
+print(os.environ.get("MAILJET_API_URL"))
+
